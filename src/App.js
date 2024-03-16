d3eeb75b86563161d5464feb09d6d4d0c384d516
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-03-16 23:18:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import List from "./components/List";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header /> <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const List = () => {
+  const [task, setTask] = useState([]);
+  return <div> Lista</div>;
+};
+
+export default List;
